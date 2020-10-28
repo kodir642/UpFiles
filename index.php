31b9af25227b00b05a6bf24c0f83200193b6bf19
kodir642 <herupangestu20@gmail.com>
2020-10-28 13:59:51 +0700

    kodir

---
+++ index.php
@@ -0,0 +1,54 @@
+<!DOCTYPE html>
+<html lang="en">
+    <head>
+        <meta charset="UTF-8">
+        <meta name="viewport" content="width=device-width, initial-scale=1.0">
+        <meta http-equiv="X-UA-Compatible" content="ie=edge">
+        <title>CLOUD COMPUTING</title>
+        <link rel="stylesheet" href="style.css">
+    </head>
+    <body>
+        <div class="main-menu">
+            <header>
+                <h1 align="center">SELAMAT DATANG </h1>
+            </header>
+
+            <div class="picture">
+                <img src="picture/logo.jpg">
+            </div>
+
+            <nav class="navigasi">
+                <ul>
+                    <li><a class="active" href="index.html">HOME</a></li>
+                    <li><a href="AboutUs.html">ABOUT US</a></li>
+                </ul>
+            </nav>
+
+            <div class="content-kiri">
+                <div class="kotak">
+                    <h3>REGISTRASI</h3>
+                    <a class="tombol-kiri" href="form_register.php">LIHAT</a>
+                </div>
+            </div>
+            <div class="content-utama">
+                <div class="kotak">
+                    <h2>CLOUD COMPUTING</h2>
+                    <img src="pict/cloud.jpg" height="200px" width="830px" alt="cloud">
+                    <p align="justify">Cloud Computing adalah gabungan pemanfaatan teknologi komputer ('komputasi') dan pengembangan berbasis Internet ('awan'). 
+                        Awan (cloud) adalah metafora dari internet, sebagaimana awan yang sering digambarkan di diagram jaringan komputer. 
+                        Sebagaimana awan dalam diagram jaringan komputer tersebut, awan (cloud) dalam Cloud Computing juga merupakan abstraksi 
+                        dari infrastruktur kompleks yang disembunyikannya. Ia adalah suatu metoda komputasi di mana kapabilitas terkait teknologi 
+                        informasi disajikan sebagai suatu layanan (as a service), sehingga pengguna dapat mengaksesnya lewat Internet ("di dalam awan") 
+                        tanpa mengetahui apa yang ada didalamnya, ahli dengannya, atau memiliki kendali terhadap infrastruktur teknologi yang membantunya. 
+                        Menurut sebuah makalah tahun 2008 yang dipublikasi IEEE Internet Computing "Cloud Computing adalah suatu paradigma di mana informasi 
+                        secara permanen tersimpan di server internet dan tersimpan secara sementara di komputer pengguna (client) termasuk di dalamnya 
+                        adalah desktop, komputer tablet, notebook, komputer dinding, handheld, sensor-sensor, monitor dan lain-lain. 
+                        ~ https://id.wikipedia.org/wiki/Komputasi_awan</p>
+                </div>
+            </div>
+        <footer>
+                STMIK AKAKOM YOGYAKARTA
+        </footer>
+        </div>
+    </body>
+</html>
