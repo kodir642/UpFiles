77d892256d29fc40bd2c0c3f49a4ef5efa712010
kodir642 <herupangestu20@gmail.com>
2020-10-28 15:38:58 +0700

    kodir

--- index.php
+++ index.php
@@ -14,7 +14,7 @@
             </header>
 
             <div class="picture">
-                <img src="picture/logo.jpg">
+                <img src="pict/bulan.jpeg">
             </div>
 
             <nav class="navigasi">
@@ -33,17 +33,15 @@
             <div class="content-utama">
                 <div class="kotak">
                     <h2>CLOUD COMPUTING</h2>
-                    <img src="pict/cloud.jpg" height="200px" width="830px" alt="cloud">
-                    <p align="justify">Cloud Computing adalah gabungan pemanfaatan teknologi komputer ('komputasi') dan pengembangan berbasis Internet ('awan'). 
-                        Awan (cloud) adalah metafora dari internet, sebagaimana awan yang sering digambarkan di diagram jaringan komputer. 
-                        Sebagaimana awan dalam diagram jaringan komputer tersebut, awan (cloud) dalam Cloud Computing juga merupakan abstraksi 
-                        dari infrastruktur kompleks yang disembunyikannya. Ia adalah suatu metoda komputasi di mana kapabilitas terkait teknologi 
-                        informasi disajikan sebagai suatu layanan (as a service), sehingga pengguna dapat mengaksesnya lewat Internet ("di dalam awan") 
-                        tanpa mengetahui apa yang ada didalamnya, ahli dengannya, atau memiliki kendali terhadap infrastruktur teknologi yang membantunya. 
-                        Menurut sebuah makalah tahun 2008 yang dipublikasi IEEE Internet Computing "Cloud Computing adalah suatu paradigma di mana informasi 
-                        secara permanen tersimpan di server internet dan tersimpan secara sementara di komputer pengguna (client) termasuk di dalamnya 
-                        adalah desktop, komputer tablet, notebook, komputer dinding, handheld, sensor-sensor, monitor dan lain-lain. 
-                        ~ https://id.wikipedia.org/wiki/Komputasi_awan</p>
+                    <img src="pict/cloud.png" height="200px" width="830px" alt="cloud">
+                    <p align="justify">loud Computing merupakan istilah dari Cloud diartikan sebagai internet 
+                    dan Computing diartikan sebagai komputer. Definisi dari Cloud Computing adalah sebuah proses 
+                    pengolahan daya komputasi  melalui jaringan internet  yang memiliki fungsi agar dapat menjalankan
+                     program melalui komputer yang telah terkoneksi satu sama lain pada waktu yang sama. Cloud Computing 
+                     merupakan sebuah teknologi yang menjadikan internet sebagai pusat server untuk mengelola data dan juga
+                      aplikasi pengguna. Cloud Computing memudahkan penggunanya untuk menjalankan program tanpa harus 
+                      menginstall aplikasi terlebih dahulu dan memudahkan pengguna untuk mengakses data dan informasi 
+                      melalui internet.</p>
                 </div>
             </div>
         <footer>
